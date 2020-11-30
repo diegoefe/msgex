3ef8f71abc68d5b110f2559cc7635b6456d76cc5
Diego F(i7) <diegoefe@gmail.com>
2020-11-30 08:03:47 -0300

    Windows fixes

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## Simple message excercise
 
 ### Setup
+
 - Install nodejs
 - Install dependencies
 
@@ -31,3 +32,12 @@
   ```
 
 - Remote deploy (TODO)
+
+
+### OS notes
+- Use this preferably on a Linux machine, on Windows you may try this command, before 'npm install', to make it work with
+  ```bash
+    npm install -g windows-build-tools.
+  ```
+  In case some pre-compiled binaries for your machine where not found and may need to be recompiled
+  
--- test/config.spec.ts
+++ test/config.spec.ts
@@ -4,15 +4,18 @@ import { Config } from '../src/config';
 import tcfg from './testconfig';
 
 describe('Config', 
-  () => { 
-    it('load from file', () => { 
+  () => {
+    it('load from file', () => {
       const cfg:Config = new Config('./sample-config.yaml');
       expect(cfg.server).to.eql(tcfg.server)
       expect(cfg.kafka).to.eql(tcfg.kafka)
-    }); 
-    it('load from environment', () => { 
+    });
+    it('load from environment', function() {
+      if(process.platform === 'win32') {
+         this.skip();
+      }
       const cfg:Config = new Config();
       expect(cfg.server).to.eql(tcfg.server)
-      expect(cfg.kafka).to.eql(tcfg.kafka)
+      expect(cfg.kafka).to.eql(tcfg.kafka)   
     }); 
 });
