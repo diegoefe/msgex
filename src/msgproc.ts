69846373c7a422361ffdc2b50c51428b61440a91
Diego F(i7) <diegoefe@gmail.com>
2020-11-30 11:06:30 -0300

    MsgProc: untested basic processing

--- src/msgproc.ts
+++ src/msgproc.ts
@@ -5,20 +5,19 @@ import { LooseObject } from './types';
 // Message
 export class Msg {
     readonly transaction_id:string;
-    readonly payload:LooseObject;
+    payload:LooseObject;
     constructor(_text:string, _data:LooseObject, _transaction_id:string) {
         this.payload = { "message":_text };
         Object.keys(_data).forEach((key:string) => {
             this.payload[key] = _data[key];
         })
-        
         this.transaction_id = _transaction_id;
     }
 };
 
 export class PingMsg extends Msg {
     constructor(_force_error:boolean, _transaction_id:string=uuidv4()) {
-        super("ping", {"force_error": _force_error}, _transaction_id);
+        super("ping", {"force_error": _force_error, "created_at": new Date()}, _transaction_id);
     }
 }
 
@@ -39,7 +38,7 @@ export interface iProducer {
 }
 
 enum Status {
-    Unsent=1, SentOK, SendErr
+    Unsent=1, SentOK, SendErr, Deleted
 }
 
 class MsgState {
@@ -53,9 +52,10 @@ class MsgState {
     }
 }
 
-interface MsgDB {
+type MsgDB = {
     [key: string]: MsgState
 };
+
 
 // Message Processor
 export class MsgProc {
@@ -70,9 +70,48 @@ export class MsgProc {
         this.prod_ = _prod;
         this.topics_ = _config.kafka.topics.outbound;
     }
-    async consume(_msg:PingMsg) {
+    private async send(_msg:PingMsg) {
+        let mt:MsgState = new MsgState(_msg);
         if(! (_msg.transaction_id in this.msgs_)) {
-            
+            this.msgs_[_msg.transaction_id] = mt;
+        }
+        try {
+            const pong:PongMsg = new PongMsg(new Date().getDate(), mt.msg.transaction_id);
+            if(mt.msg.payload.force_error) {
+                await this.prod_.send(this.topics_.error, pong);
+                mt.status = Status.SendErr;
+            } else {
+                await this.prod_.send(this.topics_.success, pong);
+                mt.status = Status.SentOK;
+            }
+            ++mt.number_of_tries;
+            if(mt.number_of_tries>=this.failure_limit) {
+                await this.prod_.send(this.topics_.dead, pong);
+                mt.status = Status.Deleted;
+            }
+        } catch(e:any) {
+
+        }
+    }
+    async consume(_msg:PingMsg) {
+        if(_msg.transaction_id in this.msgs_) {
+            let mt:MsgState = this.msgs_[_msg.transaction_id];
+            switch(mt.status) {
+                case Status.Deleted:
+                    // nothing to do
+                    break;
+                case Status.SentOK:
+                    // we respond without processing
+                    await this.send(_msg);
+                    break;
+                case Status.SendErr:
+                    // retry the failed message
+                    setTimeout(() => { this.send(_msg); }, this.delay);
+                    break;                    
+            }
+        } else {
+            // new nessage
+            setTimeout(() => { this.send(_msg); }, this.delay);
         }
     }
 }
--- test/msgproc.spec.ts
+++ test/msgproc.spec.ts
@@ -24,14 +24,16 @@ describe('Message processing',
 () => { 
   it('Msg creation', () => { 
       const ping = new PingMsg(false);
-      expect(ping.payload).to.eql({ message:"ping", force_error:false });
+      let now= ping.payload.created_at;
+      expect(ping.payload).to.eql({ message:"ping", force_error:false, created_at:now });
       const time:number=10000;
       const pong = new PongMsg(time, ping.transaction_id);
       expect(pong.payload).to.eql({ message:"pong", processing_time:time });
       expect(pong.transaction_id).to.eql(ping.transaction_id)
       const custom_tid:string = "my-custom-transaction-id";
       const ping2 = new PingMsg(true, custom_tid);
-      expect(ping2.payload).to.eql({ message:"ping", force_error:true });
+      now= ping2.payload.created_at;
+      expect(ping2.payload).to.eql({ message:"ping", force_error:true, created_at:now });
       expect(ping2.transaction_id).to.eql(custom_tid)
   });
   
