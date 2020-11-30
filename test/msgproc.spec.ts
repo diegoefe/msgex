063631f34cd9e51bdd08d1f54ec580c638b38e8c
Diego F(i7) <diegoefe@gmail.com>
2020-11-30 09:43:56 -0300

    Testing MsgProc construction

--- test/msgproc.spec.ts
+++ test/msgproc.spec.ts
@@ -1,15 +1,51 @@
 import { expect } from 'chai';
 import 'mocha';
 
-import { PingMsg, PongMsg, MsgProc } from '../src/msgproc';
+import { Msg, PingMsg, PongMsg, ErrMsg, MsgProc, iProducer } from '../src/msgproc';
+import sinon, { stubInterface,  stubConstructor } from "ts-sinon";
+import { Config } from '../src/config';
+import { resolve } from 'path';
 
-describe('Message processor', 
+
+class MockProd implements iProducer {
+  topic:string;
+  msg:Msg;
+  
+  constructor() {
+    this.topic='';
+    this.msg = new ErrMsg('');
+  }
+
+  send(_topic:string, _msg:Msg) {
+    this.topic = _topic;
+    this.msg = _msg;
+    return new Promise<string>((resolve, reject)=>{
+      resolve("ok")
+    })
+  }
+}
+
+describe('Message processing', 
 () => { 
-    it('Msg creation', () => { 
+  it('Msg creation', () => { 
       const ping = new PingMsg(false);
       expect(ping.payload).to.eql({ message:"ping", force_error:false });
       const time:number=10000;
       const pong = new PongMsg(time);
       expect(pong.payload).to.eql({ message:"pong", processing_time:time });
-  }); 
+  });
+  
+  describe('MsgProc', 
+  () => { 
+    it('instantiation', () => {
+      let dummyProc = stubConstructor(MockProd);
+      let cfg:Config = new Config('./sample-config.yaml');
+      cfg.server.messages.processing_delay = 10;
+      cfg.server.messages.processing_delay = 4;
+      let mp:MsgProc = new MsgProc(cfg, dummyProc);
+      expect(mp.delay).to.eql(cfg.server.messages.processing_delay);      
+      expect(mp.failure_limit).to.eql(cfg.server.messages.failure_limit);      
+    });
+  });
+
 });
